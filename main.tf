25a8882a33f1ba2da38f56bca85cc5381e4328c2
tomektom <57803202+tomektom@users.noreply.github.com>
2023-06-28 15:46:50 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "aws_s3_bucket_lifecycle_configuration" "bucket-config" {
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-up-and-running-locks"
+  name         = var.dynamodb_table_name
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
   attribute {
