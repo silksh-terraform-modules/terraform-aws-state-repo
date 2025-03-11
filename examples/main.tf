5da1b99c7bc74799f12c35d6ad2577726095f26b
Tomasz Warczykowski <tomasz.warczykowski@silksh.pl>
2025-03-11 15:14:59 +0100

    use s3 lock instead dynamodb

--- examples/main.tf
+++ examples/main.tf
@@ -1,6 +1,23 @@
 module "state_repo" {
-  source = "github.com/silksh-terraform-modules/terraform-aws-state-repo?ref=v0.0.1"
-  
-  repo_prefix = var.project_full_name
+  source = "github.com/silksh-terraform-modules/terraform-aws-state-repo?ref=v0.0.2"
 
+  repo_prefix = var.project_full_name
 }
+
+# backend usage
+# terraform {
+#   required_version = "~> 1.11"
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 5.90"
+#     }
+#   }
+#   backend "s3" {
+#     bucket       = "${var.project_full_name}-terraform-up-and-running-state"
+#     key          = "terraform.tfstate"
+#     region       = var.region
+#     use_lockfile = true
+#     encrypt      = true
+#   }
+# }
--- main.tf
+++ main.tf
@@ -1,10 +1,7 @@
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "${var.repo_prefix}-terraform-up-and-running-state"
-  # Enable versioning so we can see the full revision history of our
-  # state files
- 
-  force_destroy = true
-  # Enable server-side encryption by default
+  bucket              = "${var.repo_prefix}-terraform-up-and-running-state"
+  force_destroy       = true
+  object_lock_enabled = true
 
 }
 
@@ -17,34 +14,20 @@ resource "aws_s3_bucket_versioning" "terraform_state" {
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
   bucket = aws_s3_bucket.terraform_state.bucket
-
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm     = "aws:kms"
+      sse_algorithm = "aws:kms"
     }
   }
 }
-
 
 resource "aws_s3_bucket_lifecycle_configuration" "bucket-config" {
   bucket = aws_s3_bucket.terraform_state.bucket
-
   rule {
     id = "versions_expiring"
-      noncurrent_version_expiration {
-        noncurrent_days = var.noncurrent_version_expiration_days
+    noncurrent_version_expiration {
+      noncurrent_days = var.noncurrent_version_expiration_days
     }
-
     status = var.noncurrent_version_expiration_status
-  }
-}
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = var.dynamodb_table_name
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
   }
 }
